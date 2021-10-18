a802b54eb2f9f46a7fab3bc1c48cb49da6991867
Gustavo Bassaco <55561265+Gustavobassaco@users.noreply.github.com>
2021-10-18 13:25:58 -0300

    Add files via upload

---
+++ questao9/Data.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class Data{
+	
+	public int dia, mes, ano;
+	
+	public Data() {
+
+	}
+	
+	public void setData(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;	
+
+	}	
+
+}
